f260449129fd2001a5bf1719673c3ab0002683cf
wahyupriadi <wahyupriadi@outlook.co.id>
2020-08-18 11:09:19 +0700

    update cart-component

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -7,12 +7,12 @@ export default class Cart extends Component {
         return (
             <div>
                 {cartItems.length === 0
-                ? <div className="cart cart-header">Cart is empty</div>
-                : <div className="cart cart-header">You have {cartItems.length} in the cart</div>}
+                    ? <div className="cart cart-header">Cart is empty</div>
+                    : <div className="cart cart-header">You have {cartItems.length} in the cart</div>}
                 <div>
                     <div className="cart">
                         <ul className="cart-items">
-                            {cartItems.map( item => ( 
+                            {cartItems.map(item => (
                                 <li ket={item._id}>
                                     <div>
                                         <img src={item.image} alt={item.title} />
@@ -23,15 +23,29 @@ export default class Cart extends Component {
                                             {formatCurrency(item.price)} x {item.count} {" "}
                                             <button className="button" onClick={() => this.props.removeFromCart(item)}>Remove</button>
                                         </div>
-                                        
+
                                     </div>
                                 </li>
                             ))}
                         </ul>
                     </div>
+                    {cartItems.length !== 0 && (
+                        <div className="cart">
+                        <div className="total">
+                            <div>
+                                Total: {" "}
+                                {formatCurrency(cartItems.reduce((a, c) => a + (c.price * c.count), 0))}
+                            </div>
+                            <button className="button primary">
+                                Proceed
+                            </button>
+                        </div>
+                    </div>
+                    )}
+                    
                 </div>
             </div>
-          
+
         )
     }
 }
